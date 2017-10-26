3dd6f915eaf30c17dd53b352cd8649ed04307921
Taaqif <taaqifpeck@gmail.com>
2017-10-27 00:21:27 +0800

    Camera bugfix

--- app/src/main/java/ict376/murdoch/edu/au/braid/AddBookActivity.java
+++ app/src/main/java/ict376/murdoch/edu/au/braid/AddBookActivity.java
@@ -1,6 +1,7 @@
 package ict376.murdoch.edu.au.braid;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,8 @@ public class AddBookActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
         setContentView(R.layout.activity_add_book);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
