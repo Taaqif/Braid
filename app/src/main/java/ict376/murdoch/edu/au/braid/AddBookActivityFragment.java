81f50b01d14f1cce5a5cc78c1890819f768b34ae
Mitchell <mitchell.panicciari@gmail.com>
2017-10-26 23:59:32 +0800

    Add photo button, opens camera then error.

--- app/src/main/java/ict376/murdoch/edu/au/braid/AddBookActivityFragment.java
+++ app/src/main/java/ict376/murdoch/edu/au/braid/AddBookActivityFragment.java
@@ -1,6 +1,13 @@
 package ict376.murdoch.edu.au.braid;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,8 +15,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class AddBookActivityFragment extends Fragment {
     //View
@@ -31,9 +41,17 @@ public class AddBookActivityFragment extends Fragment {
     EditText mRating;
     EditText mTotalPages;
     EditText mCurrentPage;
+    ImageView mCoverThumbnail;
 
     //Button for adding book
     Button mAddButton;
+    //Button for taking image
+    Button mTakePhotoButton;
+
+    //Variable for the taking photo
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+    Uri imageUri;
 
 
     public static AddBookActivityFragment newInstance(){
@@ -61,8 +79,9 @@ public class AddBookActivityFragment extends Fragment {
         }
         View detailsFrame = getActivity().findViewById(R.id.menu_fragment_container);
 
-        // set the onclick listener for the button
+        // set the onclick listeners for the buttons
         mAddButton = (Button) getActivity().findViewById(R.id.button_addBook);
+        mTakePhotoButton = (Button) getActivity().findViewById(R.id.button_takeCoverPhoto);
 
         //Assign name to each edit text
         mTitle = (EditText) getActivity().findViewById(R.id.et_title);
@@ -74,7 +93,9 @@ public class AddBookActivityFragment extends Fragment {
         mRating = (EditText) getActivity().findViewById(R.id.et_rating);
         mTotalPages = (EditText) getActivity().findViewById(R.id.et_totalpages);
         mCurrentPage = (EditText) getActivity().findViewById(R.id.et_currentpage);
+        mCoverThumbnail = (ImageView) getActivity().findViewById(R.id.iv_coverThumbnail);
 
+        //LISTENER for ADDBOOK button
         mAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -99,6 +120,55 @@ public class AddBookActivityFragment extends Fragment {
                 Log.d("myTag", title+" "+isbn+" "+cover+" "+genre+" "+author+" "+publisher+" "+datepub+" "+" "+rating+" "+totalpages+" "+currentpage);
             }
         });
+
+        //LISTENER for TAKEPHOTO button
+        mTakePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePhoto();
+            }
+        });
+    }
+
+    //Method to open the camera
+    public void takePhoto(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.bmp");
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        getActivity().startActivityForResult(takePictureIntent, 100);
+    }
+
+    //For the taking photo
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 100:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = imageUri;
+                    getActivity().getContentResolver().notifyChange(selectedImage, null);
+                    ContentResolver cr = getActivity().getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media
+                                .getBitmap(cr, selectedImage);
+
+                        mCoverThumbnail.setImageBitmap(bitmap);
+                        Toast.makeText(getActivity(), selectedImage.toString(),
+                                Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(getActivity(), "Failed to load", Toast.LENGTH_SHORT)
+                                .show();
+                        Log.e("Camera", e.toString());
+                    }
+                }
+        }
     }
 
     private void refresh() {
